4e1f64ebac942502188edcf6bab0e0105eb551e7
Vaheloid <vlad.tyupin@yandex.ru>
2025-12-19 20:26:42 +0500

    insert data into db

--- src/database.py
+++ src/database.py
@@ -4,27 +4,17 @@ from sqlalchemy.orm import Session, sessionmaker
 from sqlalchemy import URL, create_engine, text
 from config import settings
 
+#синхронный движок
 sync_engine = create_engine(
     url=settings.DATABASE_URL_psycopg,
-    echo=False, #все коммиты буду выводиться
+    echo=True, #все коммиты буду выводиться
     #pool_size=5 #количество подключений к бд
     #max_overflow=10, #дополнительные подключения
 )
-
+#асинхронный движок
 async_engine = create_async_engine(
     url=settings.DATABASE_URL_asyncpg,
-    echo=False, #все коммиты буду выводиться
+    echo=True, #все коммиты буду выводиться
     #pool_size=5 #количество подключений к бд
     #max_overflow=10, #дополнительные подключения
 )
-
-# with sync_engine.connect() as conn:
-#     res = conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
-#     print(f"{res.first()=}")
-
-async def get_123():
-    async with async_engine.connect() as conn:
-        res = await conn.execute(text("SELECT 1,2,3 union select 4,5,6"))
-        print(f"{res.first()=}")
-
-asyncio.run(get_123())
